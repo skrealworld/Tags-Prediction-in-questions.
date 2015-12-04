2d47b8a97fefb692b37ea6161f509e3f45ed0ac7
skrealworld <sk1846@rit.edu>
2015-12-04 17:24:14 -0500

    small changes.

--- data_handleing.py
+++ data_handleing.py
@@ -5,6 +5,19 @@ Author - Sourabh Kulhare
 '''
 
 import csv
+from itertools import islice
+
+
+csvfile = open('/Users/SK_Mac/Documents/Github/NLP_data/Test.csv', 'r')
+
+testreader = csv.reader(csvfile)
+
+for x in range(1,3):
+	print(testreader[x])
+
+
+
+
 def line_stream(filename, stop=None):
     """Streams `filename` one line at a time, optionally stopping at `stop`"""
     with open(filename) as csvfile:
@@ -16,4 +29,23 @@ def title_tokenize(s, stopwords=STOPWORDS):
     """Extract valid SO tags style tokens, from string `s` excluding tokens in `STOPWORDS`"""
     return [token for token in re.findall(r'\b\w[\w#+.-]*(?<!\.$)', s.lower())
                     if token not in stopwords]
+
+
+
+
+
+
+from collections import Counter
+
+def basic_recommender(title, usefulness=usefulness):
+    return Counter({word: usefulness.get(word, 0)
+                  for word in title_tokenize(title)})
+    
+
+
+
+
+
+
+
 
